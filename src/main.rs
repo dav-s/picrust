f243d776ff2e48998c0ea584d8ea12908d726357
dav-s <davis@daviskr.com>
2020-04-23 20:14:41 -0500

    Add SquareState Enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+enum SquareState {
+    Unknown,
+    Filled,
+    None,
+    OutOfBounds,
+}
+
+impl std::fmt::Display for SquareState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::Unknown     => "?",
+            Self::Filled      => "■",
+            Self::None        => " ",
+            Self::OutOfBounds => "X"
+        })
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", SquareState::Filled);
 }
